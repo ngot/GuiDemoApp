091be5680be0e044068b35ec4625e9ed3a93933e
ngot <zhuanghengfei@gmail.com>
2017-03-21 21:32:29 +0800

    feat:update

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
 const gui = require('gui');
 const co = require('coroutine');
 const path = require('path');
+const fs = require('fs');
 const os = require('os');
 const http = require('http');
 const registry = require('registry');
 
-const exe = path.basename(os.execPath);
+// const exe = path.basename(os.execPath);
+// const IEPATH = "SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION\\";
+// function setWebviewVersion(n){
+//     registry.set(
+//         registry.CURRENT_USER,
+// 	    `${IEPATH}${exe}`,
+// 	    n,
+//         registry.DWORD
+//     );
+// }
+// setWebviewVersion(11001);
 
-// registry.set(registry.CURRENT_USER,
-// 	                   "SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION\\" + exe,
-// 	                   11000, registry.DWORD);
+gui.setVersion(12001);
 
-gui.setVersion(gui.EDGE);
-
-const webview = gui.open(path.join(__dirname, 'index.html'), { debug : true });
+const webview = gui.open(path.join(__dirname, 'index.html'), { debug: true });
 
 webview.onmessage = function (message) {
     message = JSON.parse(message);
-    if (message.name === 'error') {
-        console.error('[webview error]: ', message.param);
-    } else if (message.name === 'message') {
-        console.log('[webview message]: ', message.param);
+    dispatch(message);
+}
+
+var router = {
+    "message": function (message) {
+        console.log('[webview message]: ', message);
+    },
+    "error": function (error) {
+        console.error('[webview error]: ', error);
+    },
+    "saveToFile": function (txt) {
+        fs.writeFile(path.join(__dirname, 'demo.txt'), txt);
+        console.log('[native log]: save to local file.');
+    }
+};
+
+function dispatch(msg) {
+    if (router[msg.name] && typeof router[msg.name] === 'function') {
+        router[msg.name](msg.param);
+    } else {
+        throw new Error(`can't found handler for : ${msg.name}`);
     }
 }
 
 webview.postMessage("哈哈哈！");
 
-co.sleep(1000000);
+co.current().join();
